f7b7dc3c2ef3219e3aff7c8ae91ebdc101d99181
Razvan Ilin <razvanilin@gmail.com>
2016-10-13 09:29:12 +0100

    Initial Commit

---
+++ Coursework1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Coursework1
+//
+//  Created by Razvan Ilin on 13/10/2016.
+//  Copyright © 2016 Razvan Ilin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
